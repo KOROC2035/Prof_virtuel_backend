5f49a7be821351d925ffe9b6ffae6641b186038c
ROGER KOADERE <rogerkoadere@gmail.com>
2026-06-15 15:11:51 +0200

    Ajustement final

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["stately-wisp-8a406c.netlify.app"], 
+    allow_origins=["https://stately-wisp-8a406c.netlify.app"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
